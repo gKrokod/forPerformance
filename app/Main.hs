e12d6bf0ec840f876cfd9831951db46f38bddce3
gKrokod <t9013054362@gmail.com>
2024-03-28 08:47:28 -0700

    first comment

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module Main (main) where
 -- import Test.Tasty
 import Test.Tasty.Bench
@@ -5,23 +6,75 @@ import Lib
 import Data.Char (digitToInt)
 
 testA, testB :: Integer
-testA = 129394949293294234923492394293492349239429431111323423423423413123123123123123123111133
-testB = 129394949293294234923492
+testA = 5 
+testB = 15
+testC = 20
 
-testDigitToInt :: Integer -> Int
-testDigitToInt = sum . map digitToInt . show
+ver1 :: Integer -> Integer
+ver1 0 = 0
+ver1 n = sum $ map ( \ i -> ver1 (i-1) + 3 * i ) [1..n]
 
-testReadPure :: Integer -> Int
-testReadPure = sum . map (read . pure) . show
+ver2 :: Integer -> Integer
+ver2 = (map findmemo [0..] !!)  . fromIntegral
+  where
+    findmemo 0 = 0
+    findmemo n = sum $ map ( \ i -> ver2 (i-1) + 3 * i ) [1..n]
+
+ver3 :: Integer -> Integer
+ver3 0 = 0
+ver3 1 = 3
+ver3 n = 2 * ver3 (n - 1) + 3 * n
+
+ver4 :: Integer -> Integer
+ver4 =( ver4' !! ) . fromIntegral
+
+ver4' :: [Integer]
+ver4' = 0 : zipWith (+) (map (*2) ver4') ([3,6..])
+
+ver5 :: Integer -> Integer -> Integer -> Integer
+ver5 (-1) acc k = acc
+ver5 n acc k = ver5 (pred n) (2 * acc + fromIntegral (3 * (k - n))) k 
+
+ver6 :: Integer -> Integer 
+ver6 = (\(a,b) -> 3 * (a + b)) . ((iterate (\(a,b) -> (a + b, 1 + 2 * b)) (0,1)) !!) . pred . fromIntegral
+
+ver7 :: Integer -> Integer
+ver7 n = go n (0,1) 
+  where
+    go :: Integer -> (Integer, Integer) -> Integer 
+    go !n (!a, !b) | n==0      = 3 * (a + b) 
+                   | otherwise = go (n-1) (a + b, 1 + 2 * b)
 
 main :: IO ()
 main =
 	defaultMain
     [ bgroup
-        "sum digits of number"
-        [ bench "test DigitToInt" $ nf testDigitToInt testA,
-          bench "test ReadPure" $ nf testReadPure testA,
-         bench "test DigitToInt testB" $ nf testDigitToInt testB,
-          bench "test ReadPure testB" $ nf testReadPure testB]
+        ("x func for " ++ show testA)
+        [ bench "test ver1" $ nf ver1 testA,
+          bench "test ver2" $ nf ver2 testA,
+          bench "test ver3" $ nf ver3 testA,
+          bench "test ver4" $ nf ver4 testA,
+          bench "test ver5" $ nf ver5 testA,
+          bench "test ver6" $ nf ver6 testA,
+          bench "test ver7" $ nf ver7 testA],
+     bgroup
+        ("x func for " ++ show testB)
+        [ bench "test ver1" $ nf ver1 testB,
+          bench "test ver2" $ nf ver2 testB,
+          bench "test ver3" $ nf ver3 testB,
+          bench "test ver4" $ nf ver4 testB,
+          bench "test ver5" $ nf ver5 testB,
+          bench "test ver6" $ nf ver6 testB,
+          bench "test ver7" $ nf ver7 testB],
+     bgroup
+        ("x func for " ++ show testC)
+        [ bench "test ver1" $ nf ver1 testC,
+          bench "test ver2" $ nf ver2 testC,
+          bench "test ver3" $ nf ver3 testC,
+          bench "test ver4" $ nf ver4 testC,
+          bench "test ver5" $ nf ver5 testC,
+          bench "test ver6" $ nf ver6 testC,
+          bench "test ver7" $ nf ver7 testC]
     ]
+
 
